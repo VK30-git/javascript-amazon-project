e61d4e070e9bcd00e9cc1d8913808e3b13a7aead
VK30-git <sugumaradvocatessv30@gmail.com>
2025-08-31 13:03:22 +0530

    cart quantity made interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,11 @@ document.querySelectorAll('.js-add-to-cart').forEach( (button) => {
             quantity : 1
             });
         }
-        console.log(cart);
+        let cartquantity = 0;
+        cart.forEach ( (item) => {
+            cartquantity += item.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML = cartquantity;
+       
     });
 });
