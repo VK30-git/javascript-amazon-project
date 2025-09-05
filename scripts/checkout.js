6acbf945ee9448de48a0bf21803bbc6557d300a3
VK30-git <sugumaradvocatessv30@gmail.com>
2025-09-05 21:16:09 +0530

    delete products from cart when onclick delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js' ;
+import {cart,removeFromCart} from '../data/cart.js' ;
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 let cartHTML= '';
@@ -34,7 +34,7 @@ cart.forEach ( (cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -87,5 +87,15 @@ cart.forEach ( (cartItem) => {
             </div>
     </div>
     `;
-    document.querySelector('.js-order-summary').innerHTML = cartHTML;
+    
 }); 
+document.querySelector('.js-order-summary').innerHTML = cartHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) =>{
+    link.addEventListener('click', () =>{
+       /*  event.target.closest('.cart-item-container').remove(); */ 
+       const productId=link.dataset.productId;
+       removeFromCart(productId);
+     
+    })
+})
